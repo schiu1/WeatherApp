7ca250b3239a9ef2c6b2c33ef9a98ac16edd8f1e
Stephen <gstephenc@hotmail.com>
2023-07-08 16:45:16 -0400

    need to sign up to AccuWeather to use their API

---
+++ weatherApp.js
@@ -0,0 +1,9 @@
+function searchLocation(location){
+    //need to sign up to get apikey to use this api
+    //query with fetch using location
+    //if query does not return location, give alert() with notice
+    //if query returns location, get needed data
+    //get the id of an empty div used to show the cards
+    //change the innerHTML to show the information on a card
+    //use ${} when need to use variable
+}
